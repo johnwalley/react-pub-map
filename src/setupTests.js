5dd6a5fb43a48da15d06471445a289df0ed9057f
John Walley <john@walley.org.uk>
2019-12-24 16:01:58 +0000

    Fix default test

---
+++ src/setupTests.js
@@ -0,0 +1,16 @@
+// https://stackoverflow.com/questions/54382414/fixing-react-leaflet-testing-error-cannot-read-property-layeradd-of-null
+// Work around lack of SVG support in JSDOM
+
+const createElementNSOrig = global.document.createElementNS;
+
+global.document.createElementNS = function(namespaceURI, qualifiedName) {
+  if (
+    namespaceURI === 'http://www.w3.org/2000/svg' &&
+    qualifiedName === 'svg'
+  ) {
+    const element = createElementNSOrig.apply(this, arguments);
+    element.createSVGRect = function() {};
+    return element;
+  }
+  return createElementNSOrig.apply(this, arguments);
+};
